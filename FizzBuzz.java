ab90df32697cac818f137dd21f6a515bfd7ee59d
yhtham7 <yhtham7@gmail.com>
2025-08-21 13:02:24 +0800

    Added FizzBuzz java and class file

---
+++ FizzBuzz.java
@@ -0,0 +1,41 @@
+class FizzBuzz {
+  public static int toInt(String str) {
+    try {
+      return Integer.parseInt(str);
+    } catch (NumberFormatException e) {
+      return -1;
+    }
+  }
+
+  public static void main(String[] args) {
+    int length = 100;
+    if (args.length > 0) {
+      length = toInt(args[0]);
+    }
+    for (int i = 1; i <= length; i++) {
+      /*
+      if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println("FizzBuzz");
+      } else if (i % 3 == 0) {
+        System.out.println("Fizz");
+      } else if (i % 5 == 0) {
+        System.out.println("Buzz");
+      } else {
+        System.out.println(i);
+      }
+      */
+      String out = "";
+      if (i % 3 == 0) {
+        out += "Fizz";
+      }
+      if (i % 5 == 0) {
+        out += "Buzz";
+      }
+      if (out.equals("")) {
+        out = "" + i;
+      }
+      out += "\n";
+      System.out.print(out);
+    }
+  }
+}
